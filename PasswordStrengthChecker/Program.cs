236ffa3c6016d79dc44e534a7fa6136c2e55271e
Rich Worsley <rworsl01@dcs.bbk.ac.uk>
2021-02-18 23:34:12 +0000

    further tests created, and further steps for progression

--- PasswordStrengthChecker/Program.cs
+++ PasswordStrengthChecker/Program.cs
@@ -27,7 +27,123 @@ namespace PasswordStrengthChecker
             {
                 if (char.IsUpper(password[i])) count++;
             }
-            return (password.Length - count) * 2;
+
+            if (count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (password.Length - count) * 2;
+            }
+        }
+
+        public int CountLowercaseLetters(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i])) count++;
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (password.Length - count) * 2;
+            }
+        }
+
+        public int CountNumbers(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i])) count++;
+            }
+            return count * 4;
+        }
+
+        public int CountSymbols(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLetterOrDigit(password[i])) count++;
+            }
+
+            return (password.Length - count) * 6;
+        }
+
+        public int CountMiddleSymbols(string password)
+        {
+            var symbols = 0;
+            int count = 0;
+            for (int i = 1; i < password.Length - 1; i++)
+            {
+                if (char.IsLetterOrDigit(password[i])) symbols++;
+            }
+
+            for (int i = 1; i < password.Length - 1; i++)
+            {
+                if (char.IsDigit(password[i])) count++;
+            }
+            var total = (password.Length - 2) - symbols;
+            total += count;
+
+            return total * 2;
+        }
+
+        public int MeetsAllRequirements(string password)
+        {
+            int total = 0;
+            if (CountUppercaseLetters(password) > 0)
+            {
+                total += 1;
+            }
+            if (CountLowercaseLetters(password) > 0)
+            {
+                total += 1;
+            }
+            if (CountNumbers(password) > 0)
+            {
+                total += 1;
+            }
+            if (CountSymbols(password) > 0)
+            {
+                total += 1;
+            }
+            if (password.Length > 7)
+            {
+                total += 1;
+            }
+            return total * 2;
+        }
+
+        public int onlyLetters(string password)
+        {
+            if ((CountUppercaseLetters(password) + CountLowercaseLetters(password)) == password.Length)
+            {
+                return password.Length;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int onlyNumbers(string password)
+        {
+            if (CountNumbers(password) == password.Length)
+            {
+                return password.Length;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
--- PasswordStrengthCheckerTests2/ProgramTests.cs
+++ PasswordStrengthCheckerTests2/ProgramTests.cs
@@ -28,5 +28,75 @@ namespace PasswordStrengthChecker.Tests
             Assert.AreEqual(16, test1);
             Assert.AreEqual(6, test2);
         }
+
+        [TestMethod()]
+        public void CountLowercaseLettersTest()
+        {
+            var Program = new Program();
+            var test1 = Program.CountLowercaseLetters("123lowUP");
+            var test2 = Program.CountLowercaseLetters("aBcDe");
+            Assert.AreEqual(10, test1);
+            Assert.AreEqual(4, test2);
+        }
+
+        [TestMethod()]
+        public void CountNumbersTest()
+        {
+            var Program = new Program();
+            var test1 = Program.CountNumbers("123lowUP");
+            var test2 = Program.CountNumbers("aBcDe");
+            var test3 = Program.CountNumbers("1aBcDe1");
+            Assert.AreEqual(12, test1);
+            Assert.AreEqual(0, test2);
+            Assert.AreEqual(8, test3);
+        }
+
+        [TestMethod()]
+        public void CountSymbolsTest()
+        {
+            var Program = new Program();
+            var test1 = Program.CountSymbols("@12!£$3l!owUP");
+            var test2 = Program.CountSymbols("aBcDe");
+            var test3 = Program.CountSymbols("1a@@@B@@c####De;£$!1");
+            Assert.AreEqual(30, test1);
+            Assert.AreEqual(0, test2);
+            Assert.AreEqual(78, test3);
+        }
+
+        [TestMethod()]
+        public void CountMiddleSymbolsTest()
+        {
+            var Program = new Program();
+            var test1 = Program.CountMiddleSymbols("@1@dgsg!£34$%%fsd@1@");
+            var test2 = Program.CountMiddleSymbols("aBcDe");
+            var test3 = Program.CountMiddleSymbols("!!");
+            Assert.AreEqual(22, test1);
+            Assert.AreEqual(0, test2);
+            Assert.AreEqual(0, test3);
+        }
+
+        [TestMethod()]
+        public void MeetsAllRequirementsTest()
+        {
+            var Program = new Program();
+            var test1 = Program.MeetsAllRequirements("@1@dgSg!£34$%%fsd@1@");
+            var test2 = Program.MeetsAllRequirements("aBcDe");
+            var test3 = Program.MeetsAllRequirements("!!");
+            Assert.AreEqual(10, test1);
+            Assert.AreEqual(4, test2);
+            Assert.AreEqual(2, test3);
+        }
+
+        [TestMethod()]
+        public void onlyLettersTest()
+        {
+            var Program = new Program();
+            var test1 = Program.onlyLetters("@1@dgsg!£34$%%fsd@1@");
+            var test2 = Program.onlyLetters("aBcDe");
+            var test3 = Program.onlyLetters("!!");
+            Assert.AreEqual(0, test1);
+            Assert.AreEqual(5, test2);
+            Assert.AreEqual(0, test3);
+        }
     }
 }
