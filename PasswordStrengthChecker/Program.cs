b40ad70254ee59f2b6a56ac6366ff912f3f5a5b7
Rich <Rich@DESKTOP-O9UJLSI>
2021-02-18 21:24:20 +0000

    start with tests added

---
+++ PasswordStrengthChecker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PasswordStrengthChecker
+{
+    public class Program
+    {
+        int strengthScore = 0;
+
+        string Password1 = "sdadafsdyhgasgdfqw";
+        string Password2 = "SdadaFsdyhgasGdfqwe";
+        string Password3 = "1234";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int NumberOfCharacters(string password)
+        {
+            return (password.Length * 4);
+        }
+
+        public int CountUppercaseLetters(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsUpper(password[i])) count++;
+            }
+            return (password.Length - count) * 2;
+        }
+    }
+}
---
+++ PasswordStrengthCheckerTests2/ProgramTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordStrengthChecker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordStrengthChecker.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void NumberOfCharactersTest()
+        {
+            var Program = new Program();
+            var test1 = Program.NumberOfCharacters("1234");
+            var test2 = Program.NumberOfCharacters("abcde");
+            Assert.AreEqual(16, test1);
+            Assert.AreEqual(20, test2);
+        }
+
+        [TestMethod()]
+        public void CountUppercaseLettersTest()
+        {
+            var Program = new Program();
+            var test1 = Program.CountUppercaseLetters("123lowerUPPER");
+            var test2 = Program.CountUppercaseLetters("aBcDe");
+            Assert.AreEqual(16, test1);
+            Assert.AreEqual(6, test2);
+        }
+    }
+}
