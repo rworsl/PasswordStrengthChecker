837ae11483d60e2dde3f9961d8d53fc8b14142ec
Rich <Rich@DESKTOP-O9UJLSI>
2021-02-23 18:21:52 +0000

    scaling sorted out, now to integrate

--- PasswordStrengthChecker/Program.cs
+++ PasswordStrengthChecker/Program.cs
@@ -10,67 +10,101 @@ namespace PasswordStrengthChecker
         static void Main(string[] args)
         {
             int total = 0;
-            string password = "SullenCitadelTwiddlingCosigner";
+            int total2 = 0;
+            //string password = "apsoa5@abc";
+            //string password = "iloveyoumybubbaloo!";
+            //string password = "DispersalCharger@EnticingOverripe";
+            //string password = "P4ternityPr3natalV3nding6racious";
+            string password = "0!l7hr@$h(0nfu$ingFl4m@bl£";
 
             var Program = new Program();
 
             total += Program.NumberOfCharacters(password);
             //max 200
-            Console.WriteLine(Program.NumberOfCharacters(password));
-            
+            Console.WriteLine("NumberOfCharacters: " + total);
+
             //max 400
             total += Program.CountUppercaseLetters(password);
-            Console.WriteLine(Program.CountUppercaseLetters(password));
+            Console.WriteLine("CountUppercaseLetters: " + total);
             
             //max 100
             total += Program.CountLowercaseLetters(password);
-            Console.WriteLine(Program.CountLowercaseLetters(password));
-            
+            Console.WriteLine("CountLowercaseLetters: " + total);
+
             //max 50
             total += Program.CountNumbers(password);
-            Console.WriteLine(Program.CountNumbers(password));
-            
+            Console.WriteLine("CountNumbers: " + total);
+
             //max 50
             total += Program.CountSymbols(password);
-            Console.WriteLine(Program.CountSymbols(password));
-            
+            Console.WriteLine("CountSymbols: " + total);
+
             //max 50
             total += Program.CountMiddleSymbols(password);
-            Console.WriteLine(Program.CountMiddleSymbols(password));
-            total += Program.MeetsAllRequirements(password);
-            
+            Console.WriteLine("CountMiddleSymbols: " + total);
+
             //max 10
-            Console.WriteLine(Program.MeetsAllRequirements(password));
-            total -= Program.onlyLetters(password);
-            
+            total += Program.MeetsAllRequirements(password);
+            Console.WriteLine("MeetsAllRequirements: " + total);
+            Console.WriteLine("");
+
             //Max possible = 860
 
+            //max 200
+            total2 += Program.onlyLetters(password);
+            Console.WriteLine("onlyLetters: " + total2);
 
             //max 200
-            Console.WriteLine(Program.onlyLetters(password));
-            total -= Program.onlyNumbers(password);
+            total2 += Program.onlyNumbers(password);
+            Console.WriteLine("onlyNumbers: " + total2);
+
+            //max 50
+            total2 += Program.checkRepeatCharacters(password);
+            Console.WriteLine("checkRepeatCharacters: " + total2);
+
+            //max 50
+            total2 += Program.ConsecutiveUpperLetters(password);
+            Console.WriteLine("ConsecutiveUpperLetters: " + total2);
+
+            //max 50
+            total2 += Program.ConsecutiveLowerLetters(password);
+            Console.WriteLine("ConsecutiveLowerLetters: " + total2);
+
+            //max 50
+            total2 += Program.ConsecutiveNumbers(password);
+            Console.WriteLine("ConsecutiveNumbers: " + total2);
+
+            //max 50
+            total2 += Program.SequentialLetters(password);
+            Console.WriteLine("SequentialLetters: " + total2);
+
+            var finalTotal = Program.scaledScore(total);
+            Console.WriteLine("Scaled :" + finalTotal);
             
-            //max 200
-            Console.WriteLine(Program.onlyNumbers(password));
-            total -= Program.checkRepeatCharacters(password);
             
-            //max 50
-            Console.WriteLine(Program.checkRepeatCharacters(password));
+            Console.WriteLine("");
+            Console.WriteLine("Final total: " + (total - total2));
+        }
 
-            //max 50
-            total -= Program.ConsecutiveUpperLetters(password);
+        public double scaledScore(int total)
+        {
+            /*var oldRange = 250;
+            var newRange = 100;
+            var combined = (double)newRange / oldRange;
 
-            //max 50
-            total -= Program.ConsecutiveLowerLetters(password);
-            
-            //max 50
-            total -= Program.ConsecutiveNumbers(password);
+            var Value = (double)(total * combined);
 
-            //max 50
-            Console.WriteLine(Program.SequentialLetters(password));
-
-
-            Console.WriteLine(total);
+            Console.WriteLine(Value);*/
+            var Value = total / 2.5;
+            if (Value > 100)
+            {
+                return 100;
+            }
+            else
+            {
+                return Value;
+            }
+            return Value;
         }
 
         public int NumberOfCharacters(string password)
@@ -220,7 +254,15 @@ namespace PasswordStrengthChecker
             {
                 total += 1;
             }
-            return total * 2;
+
+            if (total <= 3)
+            {
+                return 0;
+            }
+            else
+            {
+                return total * 2;
+            }
         }
 
         public int onlyLetters(string password)
@@ -263,6 +305,7 @@ namespace PasswordStrengthChecker
 
         public int checkRepeatCharacters(string password)
         {
+            /*
             int itemCount = 0;
             List<char> empty = new List<char>();
 
@@ -294,6 +337,20 @@ namespace PasswordStrengthChecker
             {
                 return itemCount * 2;
             }
+            */
+
+            HashSet<char> characters = new HashSet<char>();
+            foreach(char i in password)
+            {
+                characters.Add(i);
+            }
+
+            int originalTotal = password.Length;
+            int newTotal = characters.Count;
+
+            int Result = originalTotal - newTotal;
+
+            return Result > 50 ? 50 : Result;
         }
 
         public int ConsecutiveUpperLetters(string password)
